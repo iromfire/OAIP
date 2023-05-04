558a940228df42e3fc1cc9363bb5adfe52b1e223
artur <green.neh@gmail.com>
2023-05-04 15:10:12 +0300

    update v1

--- OAIP/Guest.xaml.cs
+++ OAIP/Guest.xaml.cs
@@ -1,16 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+
 
 namespace OAIP
 {
@@ -28,10 +20,7 @@ namespace OAIP
             }
         }
 
-        private void dataGrid3_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
 
-        }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
